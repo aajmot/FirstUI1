30092907048ec31990deb5dba153bdc855048b61
Ajmot Sk <aajmot@gmail.com>
2018-06-26 16:13:54 +0530

    initial commit into new repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,30 @@
+import { FormsModule } from '@angular/Forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: LoginComponent },
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
